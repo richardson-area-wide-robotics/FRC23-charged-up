7040cf498c08dbcc1c778d8d9c8642afe5e6cf83
Chase Beverly <cbeverly0613@gmail.com>
2023-01-13 11:10:30 -0600

    created constant file for MAXSwerve drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,6 +9,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import frc.lib.controller.PIDGains;
+import frc.lib.util.SwerveModuleConstants;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -19,17 +21,20 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final class DriveConstants {
+  public static final class SwerveDriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
     public static final double kMaxSpeedMetersPerSecond = 4.8;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
-    // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    /*
+     * Chassis configuration
+     */
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    public static final double kTrackWidth = Units.inchesToMeters(26.5);
     // Distance between front and back wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(26.5);
+
     public static final SwerveDriveKinematics kDriveKinematics =
         new SwerveDriveKinematics(
             new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -37,22 +42,62 @@ public final class Constants {
             new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
             new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-    // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-    public static final double kFrontRightChassisAngularOffset = 0;
-    public static final double kBackLeftChassisAngularOffset = Math.PI;
-    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+    /*
+     * Spark Max and encoder constents for MAXSwerve modules
+     */
+    public static final class FrontLeftModule {
+      // The CAN ID for the drive motor
+      public static final int kDriveMotorCANID = 1;
+      // The CAN ID for the steer motor
+      public static final int kSteerMotorCANID = 2;
+      // The Angular offset in radians for the steer encoder in radians
+      public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+      // creating the swerve module constants for the front left module
+      public static final SwerveModuleConstants S_MODULE_CONSTANTS =
+          new SwerveModuleConstants(
+              kDriveMotorCANID, kSteerMotorCANID, kFrontLeftChassisAngularOffset);
+    }
 
-    // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 11;
-    public static final int kRearLeftDrivingCanId = 13;
-    public static final int kFrontRightDrivingCanId = 15;
-    public static final int kRearRightDrivingCanId = 17;
+    public static final class FrontRightModule {
+      // The CAN ID for the drive motor
+      public static final int kDriveMotorCANID = 7;
+      // The CAN ID for the steer motor
+      public static final int kSteerMotorCANID = 8;
+      // The Angular offsets in radians for the steer encoder
+      public static final double kFrontRightChassisAngularOffset = 0;
+      // creating the swerve module constants for the front right module
+      public static final SwerveModuleConstants S_MODULE_CONSTANTS =
+          new SwerveModuleConstants(
+              kDriveMotorCANID, kSteerMotorCANID, kFrontRightChassisAngularOffset);
+    }
 
-    public static final int kFrontLeftTurningCanId = 10;
-    public static final int kRearLeftTurningCanId = 12;
-    public static final int kFrontRightTurningCanId = 14;
-    public static final int kRearRightTurningCanId = 16;
+    /* Back Left Module - Module 2 */
+    public static final class BackLeftModule {
+      // The CAN ID for the drive motor
+      public static final int kDriveMotorCANID = 3;
+      // The CAN ID for the steer motor
+      public static final int kSteerMotorCANID = 4;
+      // The Angular offsets in radians for the steer encoder
+      public static final double kBackLeftChassisAngularOffset = Math.PI;
+      // creating the swerve module constants for the back left module
+      public static final SwerveModuleConstants S_MODULE_CONSTANTS =
+          new SwerveModuleConstants(
+              kDriveMotorCANID, kSteerMotorCANID, kBackLeftChassisAngularOffset);
+    }
+
+    /* Back Right Module - Module 3 */
+    public static final class BackRightModule {
+      // The CAN ID for the drive motor
+      public static final int kDriveMotorCANID = 5;
+      // The CAN ID for the steer motor
+      public static final int kSteerMotorCANID = 6;
+      // The Angular offsets in radians for the steer encoder
+      public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+      // creating the swerve module constants for the back right module
+      public static final SwerveModuleConstants S_MODULE_CONSTANTS =
+          new SwerveModuleConstants(
+              kDriveMotorCANID, kSteerMotorCANID, kBackRightChassisAngularOffset);
+    }
 
     public static final boolean kGyroReversed = false;
   }
@@ -91,16 +136,14 @@ public final class Constants {
     public static final double kTurningEncoderPositionPIDMaxInput =
         kTurningEncoderPositionFactor; // radians
 
-    public static final double kDrivingP = 0.04;
-    public static final double kDrivingI = 0;
-    public static final double kDrivingD = 0;
+    public static final PIDGains kDrivingPIDGains =
+        new PIDGains(0.04, 0, 0); // TODO: tune values for the driving motor
     public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
-    public static final double kTurningP = 1;
-    public static final double kTurningI = 0;
-    public static final double kTurningD = 0;
+    public static final PIDGains kTurningPIDGains =
+        new PIDGains(1, 0, 0); // TODO: tune values for the turning motor
     public static final double kTurningFF = 0;
     public static final double kTurningMinOutput = -1;
     public static final double kTurningMaxOutput = 1;
@@ -114,6 +157,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 0;
   }
 
   public static final class AutoConstants {

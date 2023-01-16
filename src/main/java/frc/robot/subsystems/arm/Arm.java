bc41c0de19fdad95b8dedf47be222c65b8468f13
Vim-3 <114448137+Vim-3@users.noreply.github.com>
2023-01-16 14:44:10 -0600

    Update Arm.java

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.arm;
 
+import java.util.EnumMap;
+
 //importing libraries
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -11,26 +13,32 @@ public class Arm {
     private CANSparkMax leftMotor;
     private CANSparkMax rightMotor;
 
-    //variables to set limits, speeds, and ramp-rate
-    private final float REVERSE_LIMIT = (float) 0;
-    private final float FORWARD_LIMIT = (float) 0;
-    private final double REVERSE_SPEED = 0;
-    private final double FORWARD_SPEED = 0;
-    private final double RAMPRATE = 0;
-
-    //variables to set the arm position when scoring and intaking
-    private armPosition lowScore = armPosition.SCORING_ARM_POSITION_LOW;
-    private armPosition midScore = armPosition.SCORING_ARM_POSITION_MID;
-    private armPosition groundIn = armPosition.INTAKE_ARM_POSITION_GROUND;
-    private armPosition shelfIn = armPosition.INTAKE_ARM_POSITION_SHELF;
-
     // enum sets unchangeable variables, here it sets the arm height for intaking and scoring game objects
-    enum armPosition {
-    INTAKE_ARM_POSITION_GROUND,
-    INTAKE_ARM_POSITION_SHELF,
+    public enum armPosition {
+    SCORING_ARM_POSITION_LOW,
     SCORING_ARM_POSITION_MID,
-    SCORING_ARM_POSITION_LOW
+    INTAKE_ARM_POSITION_GROUND,
+    INTAKE_ARM_POSITION_SHELF
     }
+
+    //variables to set limits, speeds
+    private final float REVERSE_LIMIT = 0.0f;
+    private final float FORWARD_LIMIT = 0.0f;
+    private final double REVERSE_SPEED = 0.0;
+    private final double FORWARD_SPEED = 0.0;
+
+    //doubles for arm positions
+    private final double INTAKE_ARM_GROUND = 0.0;
+    private final double INTAKE_ARM_SHELF = 0.0;
+    private final double SCORING_ARM_LOW = 0.0;
+    private final double SCORING_ARM_MID = 0.0;
+
+    //Map of arm positions named armPositions
+    EnumMap<armPosition, Double> armPositions 
+    = new EnumMap<>(armPosition.class);
+
+    //variable to set the arm position when scoring and intaking
+    private armPosition currentArmPosition = armPosition.INTAKE_ARM_POSITION_GROUND;
 
     //setting up CAN IDs for the motors
     public Arm(int rightMotorCANID, int leftMotorCANID) {
@@ -47,12 +55,8 @@ public class Arm {
     this.rightMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
     this.rightMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, REVERSE_LIMIT);
 
-    //setting the ramp-rate for open loop modules (the max amount the motor controller's output is allowed to change)
-    this.rightMotor.setOpenLoopRampRate(RAMPRATE);
-
     //setting the idle mode setting for the SparkMax, here it is set to brake when idle
     this.rightMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
-
 
     //motor type for left motor
     this.leftMotor = new CANSparkMax(leftMotorCANID, MotorType.kBrushless);
@@ -61,27 +65,33 @@ public class Arm {
     // this.leftMotor.setInverted(invertLeftMotor);
 
     this.leftMotor.follow(this.rightMotor, true);
+    
+    armPositions.put(armPosition.INTAKE_ARM_POSITION_GROUND, INTAKE_ARM_GROUND);
+    armPositions.put(armPosition.INTAKE_ARM_POSITION_SHELF, INTAKE_ARM_SHELF);
+    armPositions.put(armPosition.SCORING_ARM_POSITION_LOW, SCORING_ARM_LOW);
+    armPositions.put(armPosition.SCORING_ARM_POSITION_MID, SCORING_ARM_MID);
+
   }
 
   //setting functions of runToScore and runToIntake
   public void runToScoreLow() {
     this.rightMotor.set(FORWARD_SPEED);
-    this.lowScore = armPosition.SCORING_ARM_POSITION_LOW;
+    this.currentArmPosition = armPosition.SCORING_ARM_POSITION_LOW;
   }
 
   public void runToScoreMid() {
     this.rightMotor.set(FORWARD_SPEED);
-    this.midScore = armPosition.SCORING_ARM_POSITION_MID;
+    this.currentArmPosition = armPosition.SCORING_ARM_POSITION_MID;
   }
 
   public void runToIntakeGround() {
     this.rightMotor.set(REVERSE_SPEED);
-    this.groundIn = armPosition.INTAKE_ARM_POSITION_GROUND;
+    this.currentArmPosition = armPosition.INTAKE_ARM_POSITION_GROUND;
   }
 
   public void runToIntakeShelf() {
     this.rightMotor.set(REVERSE_SPEED);
-    this.shelfIn = armPosition.INTAKE_ARM_POSITION_SHELF;
+    this.currentArmPosition = armPosition.INTAKE_ARM_POSITION_SHELF;
   }
 
   //getting and positions, speed, and limits and returning them to us
@@ -106,24 +116,8 @@ public class Arm {
   }
 
 
-  //confusion, code works for now, but need to find out how to make runToScore and runToIntake choose between--
-  //--low and mid, and ground and shelf based on player input
-  public void moveArmToPosition(armPosition position) {
-    if (position == armPosition.INTAKE_ARM_POSITION_GROUND) {
-      this.runToScoreMid();
-      this.runToScoreLow();
-      }
-      else if (position == armPosition.INTAKE_ARM_POSITION_SHELF) {
-      this.runToScoreMid();
-      this.runToScoreLow();
-      }
-      else if (position == armPosition.SCORING_ARM_POSITION_LOW) {
-      this.runToIntakeGround();
-      this.runToIntakeShelf();
-      }
-      else if (position == armPosition.SCORING_ARM_POSITION_MID) {
-      this.runToIntakeShelf();
-      this.runToIntakeGround();
-      }
+  //arm movement controls using PID loop
+  public void moveArmToPosition() {
+    rightMotor.getPIDController().setReference(armPositions.get(currentArmPosition), CANSparkMax.ControlType.kPosition);
     } 
 }

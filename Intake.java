7645f45395b1bce0520651bea2663ef62d075f44
DoTheStevenSun <122140226+DoTheStevenSun@users.noreply.github.com>
2023-01-17 21:22:29 -0600

    Add files via upload

---
+++ Intake.java
@@ -0,0 +1,30 @@
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake {
+//the guys who said fortnite balls is crazy fr fr ong
+
+enum intakePostition {
+    INTAKE_POSITION,
+    INTAKE_CLAW_OPEN,
+    INTAKE_CLAW_MID-OPEN,
+    INTAKE_CLAW_CLOSE
+
+}
+private CANSparkMax intakeMotor;
+
+  private final double FORWARD_SPEED = 0.6;
+  private final double REVERSE_SPEED = -1;
+  private final double RAMPRATE = 0;
+
+
+
+
+
+
+
+
+
+}

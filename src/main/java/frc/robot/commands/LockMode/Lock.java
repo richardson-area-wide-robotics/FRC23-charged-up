4da572dcaaf71c2035ee7cd1a375adc3ffafa041
YedaniR <113949146+YedaniR@users.noreply.github.com>
2023-01-21 16:20:46 -0600

    Sent controller inputs to Lock

--- src/main/java/frc/robot/commands/LockMode/Lock.java
+++ src/main/java/frc/robot/commands/LockMode/Lock.java
@@ -1,26 +1,27 @@
 package frc.robot.commands.LockMode;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 
 public class Lock extends CommandBase{
     
-    DriveSubsystem drive ;
+    DriveSubsystem drive;
+    DoubleSupplier sideWays;
+    DoubleSupplier forward;
     boolean lockedOn = false;
     
     PIDController yawRateController = new PIDController(1, 0, 0);
 
-    public Lock(DriveSubsystem drive){
+    public Lock(DriveSubsystem drive, DoubleSupplier forward, DoubleSupplier sideWays){
         this.drive = drive;
+        this.forward = forward;
+        this.sideWays = sideWays;
     }
+
+
 
     //Changing yaw might be the best way to rotate the robot.
     @Override
@@ -34,7 +35,7 @@ public class Lock extends CommandBase{
     public void execute(){
         double angularOffset = 0;
         double yawRate = yawRateController.calculate(angularOffset, 0);
-        drive.drive(0, 0, yawRate, true);
+        drive.drive(forward.getAsDouble(), sideWays.getAsDouble(), yawRate, true);
     }
     
     

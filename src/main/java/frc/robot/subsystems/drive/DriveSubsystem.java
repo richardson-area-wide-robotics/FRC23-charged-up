04e9cfab844ce027538317f60e541576866c792d
Chase Beverly <cbeverly0613@gmail.com>
2023-01-22 23:42:38 -0600

    created trajectory command in drive

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -5,19 +5,23 @@
 package frc.robot.subsystems.drive;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.swerve.MAXSwerve;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends MAXSwerve {
-  // public Command trajectoryFollowerCommand(PathPlannerTrajectory trajectory) {
-  //   Constants.AutoConstants.kPThetaController.enableContinuousInput(-Math.PI, Math.PI);
-  //   return trajectoryFollowerCommand(
-  //       trajectory,
-  //       Constants.Drivetrain.kXController,
-  //       Constants.Drivetrain.kYController,
-  //       Constants.Drivetrain.kThetaController);
-  // }
+  public Command trajectoryFollowerCommand(PathPlannerTrajectory trajectory) {
+    Constants.AutoConstants.kPThetaController.enableContinuousInput(-Math.PI, Math.PI);
+    return trajectoryFollowerCommand(
+        trajectory,
+        Constants.AutoConstants.kPXController,
+        Constants.AutoConstants.kPYController,
+        Constants.AutoConstants.kPThetaController);
+  }
 
   static final MAXSwerveModule frontLeft =
       new MAXSwerveModule(Constants.SwerveDriveConstants.FrontLeftModule.S_MODULE_CONSTANTS);
@@ -48,12 +52,12 @@ public class DriveSubsystem extends MAXSwerve {
         Constants.SwerveDriveConstants.kMaxSpeedMetersPerSecond);
   }
 
-  // TODO: Add sendable data for controllers during autonomous mode
-  // @Override
-  // public void initSendable(SendableBuilder builder) {
-  //   super.initSendable(builder);
-  //   addChild("X Controller", Constants.SwerveConstants.kXController);
-  //   addChild("Y Controller", Constants.SwerveConstants.kYController);
-  //   addChild("Theta Controller", Constants.SwerveConstants.kThetaController);
-  // }
+  //TODO: Add sendable data for controllers during autonomous mode
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+    addChild("X Controller", Constants.AutoConstants.kPXController);
+    addChild("Y Controller", Constants.AutoConstants.kPYController);
+    addChild("Theta Controller", Constants.AutoConstants.kPThetaController);
+  }
 }

b6e5e6ee83f036585a94dd91cf60b217520978d2
YedaniR <113949146+YedaniR@users.noreply.github.com>
2023-01-23 19:24:54 -0600

    Created a lock-on class

---
+++ src/main/java/frc/robot/subsystems/Camera/Camera.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Camera;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class Camera extends SubsystemBase {
+  PhotonCamera camera;
+
+  public Camera() {
+    this.camera = new PhotonCamera("slotheye");
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {}
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public Transform3d getDistanceAndAngle() {
+    PhotonPipelineResult result = camera.getLatestResult();
+    PhotonTrackedTarget target = result.getBestTarget();
+    Transform3d pose = target.getBestCameraToTarget();
+    return pose;
+  }
+}

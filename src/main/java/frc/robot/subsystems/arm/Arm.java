044fa7d6242612d4099a779e419d283268b12d06
Chase Beverly <cbeverly0613@gmail.com>
2023-01-26 10:58:55 -0600

    added missing position

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -25,7 +25,8 @@ public class Arm extends SubsystemBase {
     SCORING_ARM_POSITION_LOW,
     SCORING_ARM_POSITION_MID,
     INTAKE_ARM_POSITION_GROUND,
-    INTAKE_ARM_POSITION_SHELF
+    INTAKE_ARM_POSITION_SHELF,
+    INTAKE_ARM_POSITION_STOWED
   }
 
   // Map of arm positions named armPositions
@@ -90,53 +91,34 @@ public class Arm extends SubsystemBase {
     armPositions.put(armPosition.INTAKE_ARM_POSITION_SHELF,  Constants.ArmConstants.INTAKE_ARM_SHELF);
     armPositions.put(armPosition.SCORING_ARM_POSITION_LOW,  Constants.ArmConstants.SCORING_ARM_LOW);
     armPositions.put(armPosition.SCORING_ARM_POSITION_MID, Constants.ArmConstants. SCORING_ARM_MID);
-  }
-
-  // setting functions of runToScore and runToIntake
-  public void runToScoreLow() {
-    this.rightMotor.set(Constants.ArmConstants.FORWARD_SPEED);
-    this.currentArmPosition = armPosition.SCORING_ARM_POSITION_LOW;
-  }
-
-  public void runToScoreMid() {
-    this.rightMotor.set(Constants.ArmConstants.FORWARD_SPEED);
-    this.currentArmPosition = armPosition.SCORING_ARM_POSITION_MID;
-  }
-
-  public void runToIntakeGround() {
-    this.rightMotor.set(Constants.ArmConstants.REVERSE_SPEED);
-    this.currentArmPosition = armPosition.INTAKE_ARM_POSITION_GROUND;
-  }
-
-  public void runToIntakeShelf() {
-    this.rightMotor.set(Constants.ArmConstants.REVERSE_SPEED);
-    this.currentArmPosition = armPosition.INTAKE_ARM_POSITION_SHELF;
+    armPositions.put(armPosition.INTAKE_ARM_POSITION_STOWED,  Constants.ArmConstants.INTAKE_ARM_STOWED);
   }
 
   // getting and positions, speed, and limits and returning them to us
   public double getPosition() {
-    return this.rightMotor.getEncoder().getPosition(); // this needs to change as we will be using external encoder for arm
+    return this.leftMotor.getEncoder().getPosition(); // this needs to change as we will be using external encoder for arm
   }
 
   public double getMeasurement() {
     return this.getPosition();
   }
-
-  public Boolean atForwardLimit() {
-    return this.rightMotor.getFault(CANSparkMax.FaultID.kSoftLimitFwd);
+  
+  public double getSpeed() {
+    return this.leftMotor.get();
   }
 
-  public double getSpeed() {
-    return this.rightMotor.get();
+  public Boolean atForwardLimit() {
+    return this.leftMotor.getFault(CANSparkMax.FaultID.kSoftLimitFwd);
   }
 
   public Boolean atReverseLimit() {
-    return this.rightMotor.getFault(CANSparkMax.FaultID.kSoftLimitRev);
+    return this.leftMotor.getFault(CANSparkMax.FaultID.kSoftLimitRev);
   }
 
   // arm movement controls using PID loop
-  public void moveArmToPosition() {
-    rightMotor
+  public void moveArmToPosition(armPosition position) {
+    currentArmPosition = position;
+    leftMotor
         .getPIDController()
         .setReference(armPositions.get(currentArmPosition), CANSparkMax.ControlType.kPosition);
   }

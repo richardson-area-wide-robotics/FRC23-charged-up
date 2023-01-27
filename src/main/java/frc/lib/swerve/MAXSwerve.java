192269d6495e4f9eed2b819f51845510035ffc4b
Chase Beverly <cbeverly0613@gmail.com>
2023-01-27 13:18:03 -0600

    fixed comments

--- src/main/java/frc/lib/swerve/MAXSwerve.java
+++ src/main/java/frc/lib/swerve/MAXSwerve.java
@@ -213,7 +213,7 @@ public class MAXSwerve extends SubsystemBase {
     m_backRight.setDesiredState(swerveModuleStates[3]);
   }
 
-  /* These methods will prevent module movement is no command is being executed on the wheels */
+  /* These methods will prevent module movement if no command is being executed on the wheels */
   private void holdModuleRotation(MAXModule m) {
     var state = m.getDesiredState();
     state.speedMetersPerSecond = 0.0;

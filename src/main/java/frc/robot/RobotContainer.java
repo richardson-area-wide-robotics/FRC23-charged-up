b0301cc58e8650423d37e26da7e0bea3f0bc94cc
PingPong27 <115889117+PingPong27@users.noreply.github.com>
2023-02-18 11:58:40 -0600

    Fixed Changes in RobotContainer
    Fixed Code by Nathan's comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,7 @@ public class RobotContainer {
   private ADIS16470_IMU m_gyro = new ADIS16470_IMU();
   private final DriveSubsystem m_robotDrive = new DriveSubsystem(m_gyro);
   private final Intake intake = new Intake();
-  // private final Arm m_arm = new Arm();
+  private final Arm m_arm = new Arm();
   private final Localizer m_localizer;
 
   // The driver's controller
@@ -88,18 +88,15 @@ public class RobotContainer {
 
           //Make sure that all buttons are unique
 
-    // new JoystickButton(m_driverController, XboxController.Button.kLeftBumper.value).onTrue(new InstantCommand(() -> intake.toggleIntake(), intake));
+    new JoystickButton(m_driverController, XboxController.Button.kLeftBumper.value).onTrue(new InstantCommand(() -> intake.toggleIntake(), intake));
 
-    // new JoystickButton(m_driverController, XboxController.Button.kY.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.INTAKE_ARM_POSITION_GROUND), m_arm));
+    new JoystickButton(m_driverController, XboxController.Button.kY.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.INTAKE_ARM_POSITION_GROUND), m_arm));
 
-    // new JoystickButton(m_driverController, XboxController.Button.kA.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.INTAKE_ARM_POSITION_STOWED), m_arm));
+    new JoystickButton(m_driverController, XboxController.Button.kA.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.INTAKE_ARM_POSITION_STOWED), m_arm));
 
-    // new JoystickButton(m_driverController, XboxController.Button.kB.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.SCORING_ARM_POSITION_MID), m_arm));
-
-    int gridPosition = 4;
+    new JoystickButton(m_driverController, XboxController.Button.kB.value).whileTrue(new InstantCommand(() -> m_arm.moveArmToPosition(armPosition.SCORING_ARM_POSITION_MID), m_arm));
     
-    
-    // new JoystickButton(m_driverController, XboxController.Button.kRightBumper).whileTrue(new InstantCommand(()-> m_driverController.driveDistance(m_localizer.get)))
+    new JoystickButton(m_driverController, XboxController.Button.kRightBumper).whileTrue(new InstantCommand(()-> m_driverController.driveDistance(m_localizer.get)))
   
   }
 

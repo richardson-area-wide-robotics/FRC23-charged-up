f77e3c186fcbbb89b38e013543f7df570b2aacdb
Robochase21 <Cbeverly0613@gmail.com>
2023-02-25 21:30:04 -0600

    cleaned imports

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems.drive;
 
-import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import frc.lib.swerve.MAXSwerve;

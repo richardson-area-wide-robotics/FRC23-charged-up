c360bef45df55646a150a88bee6ac28bc2a82db0
Robochase21 <Cbeverly0613@gmail.com>
2023-02-26 10:47:33 -0600

    finish set up with intake command

---
+++ src/main/java/frc/robot/commands/intakeCommands/IntakeCommand.java
@@ -0,0 +1,52 @@
+package frc.robot.commands.intakeCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+
+public class IntakeCommand extends CommandBase {
+    private Intake intakeMech;
+    private double intake;
+    private double outake;
+    private boolean mode; // default mode is cone mode is false, cube mode is true
+
+    public IntakeCommand(double intake, double outake, boolean mode) {
+        this.intake = intake;
+        this.outake = outake * -1;
+        this.mode = mode;  
+        addRequirements(intakeMech);
+    }
+
+    /**  use initialize to check for mode and invert the suppliers if needed, 
+    ** EX: mode cone using intake supplier to intake positive speed, while cube must have intake supplier turn negative to intake */
+    @Override
+    public void initialize() {
+        if (!mode) {
+            this.intake = Math.abs(intake);
+            this.outake = -1 * Math.abs(outake);
+        }
+        else {
+            this.intake = -1 * Math.abs(intake);
+            this.outake = Math.abs(outake);
+        }
+    }
+
+    @Override
+    public void execute(){
+        intakeMech.intake(intake);
+        intakeMech.outake(outake);
+    }
+
+    // end method will set motors to a low idle speed based on the mode
+    @Override
+    public void end(boolean interrupted) {
+        if (!mode) {
+            intakeMech.intake(0.1);
+        }
+        else {
+            intakeMech.intake(-0.1);
+        }
+    }
+
+
+
+}

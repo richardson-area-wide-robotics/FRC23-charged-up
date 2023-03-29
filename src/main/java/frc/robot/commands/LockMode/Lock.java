04c9780279029d41ea487d4ba3a0aa9050314f10
Chase Beverly <cbeverly0613@gmail.com>
2023-03-29 17:58:02 -0500

    updated

--- src/main/java/frc/robot/commands/LockMode/Lock.java
+++ src/main/java/frc/robot/commands/LockMode/Lock.java
@@ -4,7 +4,7 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.subsystems.Camera.Camera;
+import frc.robot.subsystems.camera.Camera;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 
